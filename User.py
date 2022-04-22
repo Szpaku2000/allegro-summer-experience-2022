975588aa2f78358e73618ea79fd44a1add81bff3
kszpakowski <kasz21pl@gmail.com>
2022-04-22 23:31:36 +0200

    Skeleton in progress

---
+++ User.py
@@ -0,0 +1,15 @@
+
+class User:
+
+    def __init__(self, login, name, bio, languages):
+        self.login = login
+        self.name = name
+        self.bio = bio
+        self.languages = languages
+
+
+
+
+
+
+
