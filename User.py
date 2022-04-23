8c370d6b3c1e5fbb05a831618d5efd64824b5e50
kszpakowski <kasz21pl@gmail.com>
2022-04-23 06:29:50 +0200

    User __str__ method implemented

--- User.py
+++ User.py
@@ -2,12 +2,13 @@
 class User:
 
     def __init__(self, login, name, bio, languages):
-        self.login = login
-        self.name = name
-        self.bio = bio
-        self.languages = languages
+        self.login = login                  # string - user's login (username)
+        self.name = name                    # string - user's first and last name
+        self.bio = bio                      # string - user's bio
+        self.languages = languages          # dictionary (string -> int) - language name and number of bytes that it takes in repository
 
-
+    def __str__(self):
+        return "[ Login: " + self.login + ", Name: " + self.name + ", Bio: " + self.bio + "Languages: " + self.languages + "]"
 
 
 
