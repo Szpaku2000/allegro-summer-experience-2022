3ba6477e4cd228398635c8dba57cf51f7f858cdf
kszpakowski <kasz21pl@gmail.com>
2022-04-23 06:15:04 +0200

    Comments regarding variables

---
+++ Repository.py
@@ -0,0 +1,9 @@
+
+class Repository:
+
+    def __init__(self, name,  languages):
+        self.name = name                    # string - repository name
+        self.languages = languages          # dictionary (string -> int) - language name and number of bytes that it takes in repository
+
+
+
