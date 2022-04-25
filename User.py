7039ec915315746715c1fd7dca38897228b97626
kszpakowski <kasz21pl@gmail.com>
2022-04-25 14:31:07 +0200

    userFromUsername

--- User.py
+++ User.py
@@ -4,24 +4,24 @@ import Repository
 def userFromUsername(username):
     url = 'https://api.github.com/users/'
     url = url + username
-    req = requests.get(url)
-    # TODO
-    # login =
-    # name =
-    # bio =
-    # languages =
+    req = requests.get(url).json()
+    login = req["login"]
+    name = req["name"]
+    bio = req["bio"]
+    repositoryList = User.createRepoList(name)
+    languages = User.createLangDict(repositoryList)
 
-    return User()
+    return User(login, name, bio, repositoryList, languages)
 
 
 class User:
 
-    def __init__(self, login, name, bio):
+    def __init__(self, login, name, bio, repoList, langs):
         self.login = login  # string - user's login (username)
         self.name = name  # string - user's first and last name
         self.bio = bio  # string - user's bio
-        self.repositoryList = self.createRepoList(name) # Repository[] - list of repositories of a user, it is useful in when creating languages for a user
-        self.languages = self.createLangDict(self.repositoryList) # dictionary (string -> int) - language name and number of bytes that it takes in repository
+        self.repositoryList = repoList # Repository[] - list of repositories of a user, it is useful in when creating languages for a user
+        self.languages = langs # dictionary (string -> int) - language name and number of bytes that it takes in repository
 
     def __str__(self):
         return "[ Login: " + self.login + ", Name: " + self.name + ", Bio: " + self.bio + "Languages: " + self.languages + "]"
