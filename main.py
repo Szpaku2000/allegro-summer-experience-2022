7047eb5b75fb464859a5ea2441c2c307a8ec4a40
kszpakowski <kasz21pl@gmail.com>
2022-04-26 03:34:18 +0200

    trying to make package pip installable

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ ja = User.userFromUsername("Szpaku2000")
 
 print(ja)
 
-ja.repositoryList[0].uploadRepo('https://httpbin.org/put')
-ja.uploadUser('https://httpbin.org/put')
+ja.postRepoList('https://httpbin.org/put')
+ja.postUser('https://httpbin.org/put')
 
 
 
