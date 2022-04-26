3a835f3573695dc0439944d3e22b5b1e8459d8ed
kszpakowski <kasz21pl@gmail.com>
2022-04-26 03:10:24 +0200

    trying to make package pip installable

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
 setuptools.setup(name='UserRepositoryData',
+                 version='1.0.0',
                  description='A allegro-summer-experience-2022 Python package',
                  url='https://github.com/Szpaku2000/allegro-summer-experience-2022',
-                 author='Karol Szpakowski',
                  packages=['UserRepositoryData'],
                  install_requires=['requests'])
